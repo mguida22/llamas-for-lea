cbd45f0acfaa38d2b428fa0f703297a2ce67d691
mguida22 <michael.guida@colorado.edu>
2017-01-31 11:57:17 -0700

    cache llamas for faster llama loads

--- llama.js
+++ llama.js
@@ -1,3 +1,13 @@
+function storePictureUrls(urls) {
+  chrome.storage.sync.set({ "urls": urls });
+}
+
+function getPictureUrls(cb) {
+  chrome.storage.sync.get(["urls"], function(items) {
+    cb(items.urls);
+  })
+}
+
 function httpGetAsync(url, callback) {
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.onreadystatechange = function() {
@@ -8,26 +18,44 @@ function httpGetAsync(url, callback) {
   xmlHttp.send(null);
 }
 
-function setPicture() {
-  var URL = 'https://api.flickr.com/services/rest/' +
-            '?method=flickr.groups.pools.getPhotos' +
-            '&api_key=41ec740b69920fb9fd08fca4c5bfd412' +
-            '&group_id=54097770@N00' +
-            '&per_page=500' +
-            '&extras=url_l' +
-            '&format=json' +
-            '&content_type=1' +
-            '&nojsoncallback=1';
+function setPicture(urls) {
+  var img = document.querySelector("#llama-img");
+  img.setAttribute("src", urls.pop());
 
-  httpGetAsync(URL, function(data) {
-    var photos = data.photos.photo.filter(function(photo) { return photo.url_l });
+  storePictureUrls(urls);
+}
 
-    var rand_index = Math.floor(Math.random() * (photos.length - 1));
+function main() {
+  getPictureUrls(function(urls) {
+    if (urls && urls.length > 0) {
+      setPicture(urls);
+    } else {
+      var pageIndex = Math.floor(Math.random() * 5) + 1;
+      var URL = 'https://api.flickr.com/services/rest/' +
+                '?method=flickr.groups.pools.getPhotos' +
+                '&api_key=41ec740b69920fb9fd08fca4c5bfd412' +
+                '&group_id=54097770@N00' +
+                '&per_page=500' +
+                '&page=' + pageIndex +
+                '&extras=url_l' +
+                '&format=json' +
+                '&content_type=1' +
+                '&nojsoncallback=1';
 
-    var img = document.querySelector("#llama-img");
+      httpGetAsync(URL, function(data) {
+        var photos = data.photos.photo.filter(function(photo) {
+          return (photo.url_l && photo.title.toLowerCase().indexOf("llama") > -1);
+        });
 
-    img.setAttribute("src", photos[rand_index].url_l);
+        var urls = photos.map(function(photo) {
+          return photo.url_l;
+        });
+
+        setPicture(urls);
+        storePictureUrls(urls);
+      });
+    }
   });
 }
 
-setPicture();
+main();
