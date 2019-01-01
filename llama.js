77f1a879d827329421dc7946b652adc28a59730b
Michael Guida <michael.guida@colorado.edu>
2019-01-01 17:19:40 -0500

    add history

--- llama.js
+++ llama.js
@@ -1,12 +1,25 @@
 'use strict'
 
-const storePictureData = (data) => {
-  chrome.storage.local.set({ data })
+const storePictureData = (pictureData) => {
+  chrome.storage.local.set({ pictureQueue: pictureData })
+}
+
+const storePictureDataById = (pictureData) => {
+  chrome.storage.local.get(['oldPictures'], (items) => {
+    let update = { [pictureData.id]: pictureData }
+    chrome.storage.local.set({ oldPictures: { ...update, ...items.oldPictures } })
+  })
 }
 
 const getPictureData = (callback) => {
-  chrome.storage.local.get(['data'], function (items) {
-    callback(items.data)
+  chrome.storage.local.get(['pictureQueue'], (items) => {
+    callback(items.pictureQueue)
+  })
+}
+
+const getPictureDataFromId = (id, callback) => {
+  chrome.storage.local.get(['oldPictures'], (items) => {
+    callback(items.oldPictures[id])
   })
 }
 
@@ -35,65 +48,90 @@ const httpGetAsync = (url, callback) => {
   xmlHttp.send()
 }
 
-const setPicture = (data) => {
+const updateHistory = (id) => {
+  const url = new URL(window.location.href)
+  url.search = `id=${id}`
+
+  // set the history so we can return to this specific image when pressing back
+  window.history.replaceState(null, null, url.href)
+}
+
+const setPicture = (imgData) => {
   const imgElem = document.querySelector('#llama-img')
   const titleElem = document.querySelector('#llama-title')
   const headTitleElem = document.querySelector('title')
-
-  const imgData = data.pop()
 
   imgElem.setAttribute('src', imgData.url)
   titleElem.setAttribute('href', imgData.flickrLink)
   titleElem.innerHTML = imgData.title
   headTitleElem.innerHTML = imgData.title
 
+  updateHistory(imgData.id)
+}
+
+const handlePictureUpdate = (data) => {
+  const photo = data.pop()
+  setPicture(photo)
+  storePictureDataById(photo)
   storePictureData(data)
 }
 
 const main = () => {
-  getPictureData((data) => {
-    if (data && data.length > 0) {
+  // if there's an image id param, display that image.
+  // this is to handle a user going back to an old image via history
+  const loc = new URL(window.location.href)
+  const id = loc.searchParams.get('id')
+
+  if (id) {
+    getPictureDataFromId(id, (data) => {
       setPicture(data)
-    } else {
-      const pageIndex = Math.floor(Math.random() * 5) + 1
-      const URL = 'https://api.flickr.com/services/rest/' +
-                '?method=flickr.groups.pools.getPhotos' +
-                '&api_key=41ec740b69920fb9fd08fca4c5bfd412' +
-                '&group_id=54097770@N00' +
-                '&per_page=500' +
-                '&page=' + pageIndex +
-                '&extras=url_l' +
-                '&format=json' +
-                '&content_type=1' +
-                '&nojsoncallback=1'
+    })
+  } else {
+    getPictureData((data) => {
+      if (data && data.length > 0) {
+        handlePictureUpdate(data)
+      } else {
+        const pageIndex = Math.floor(Math.random() * 5) + 1
+        const URL = 'https://api.flickr.com/services/rest/' +
+                  '?method=flickr.groups.pools.getPhotos' +
+                  '&api_key=41ec740b69920fb9fd08fca4c5bfd412' +
+                  '&group_id=54097770@N00' +
+                  '&per_page=500' +
+                  '&page=' + pageIndex +
+                  '&extras=url_l' +
+                  '&format=json' +
+                  '&content_type=1' +
+                  '&nojsoncallback=1'
 
-      httpGetAsync(URL, (response) => {
-        const photos = response.photos.photo.filter((photo) => {
-          return (
-            photo.id &&
-            photo.url_l &&
-            photo.owner &&
-            photo.title
-            // most of the 'higher quality' photos don't have numbers in the title
-            // !(/^d+$/.test(photo.title))
-          )
+        httpGetAsync(URL, (response) => {
+          const photos = response.photos.photo.filter((photo) => {
+            return (
+              photo.id &&
+              photo.url_l &&
+              photo.owner &&
+              photo.title
+              // most of the 'higher quality' photos don't have numbers in the title
+              // !(/^d+$/.test(photo.title))
+            )
+          })
+
+          let photoData = photos.map((photo) => {
+            const link = `https://www.flickr.com/photos/${photo.owner}/${photo.id}`
+            return {
+              id: photo.id,
+              url: photo.url_l,
+              title: photo.title,
+              flickrLink: link
+            }
+          })
+
+          // we don't want lots of similar pictures one after the other
+          photoData = shuffleArray(photoData)
+          handlePictureUpdate(photoData)
         })
-
-        let photoData = photos.map((photo) => {
-          const link = `https://www.flickr.com/photos/${photo.owner}/${photo.id}`
-          return {
-            url: photo.url_l,
-            title: photo.title,
-            flickrLink: link
-          }
-        })
-
-        // we don't want lots of similar pictures one after the other
-        photoData = shuffleArray(photoData)
-        setPicture(photoData)
-      })
-    }
-  })
+      }
+    })
+  }
 }
 
 main()
